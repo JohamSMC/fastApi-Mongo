a2724481b7f1d56755d0e058a89805de3ca6b0de
Joham Sebastian Medina Corredor <37983099+JohamSMC@users.noreply.github.com>
2022-06-24 18:40:18 -0500

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-conn = MongoClient("mongodb://user123:user123@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+conn = MongoClient()
 
 
